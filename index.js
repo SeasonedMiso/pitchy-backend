2b7142930f2ee6f0e21d610892c83eec50e953f1
Zak <zakwrn@gmail.com>
2021-04-20 20:03:24 +0200

    vercel bug fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 80;
 const data = require("./sentencestest.json");
 const cors = require("cors");
 const MeCab = new require("mecab-async"),
